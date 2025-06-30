e119e0e191c177c1f30843bfd48fdaf53feef676
Ubuntu <ubuntu@ip-10-0-6-246.eu-west-2.compute.internal>
2025-06-30 06:04:28 +0000

    decoding issue resolved

--- src/module/bets/bets-db.ts
+++ src/module/bets/bets-db.ts
@@ -6,7 +6,8 @@ VALUES (?, ?, ?, ?, ?, ?, ?, ?);`
 export const addSettlement = async (settlement: ISettlementDbObject): Promise<void> => {
   try {
     const { lobby_id, user_id, operator_id, bet_amount, win_amount, user_bets, win_pos, color } = settlement;
-    await write(SQL_INSERT_SETTLEMENTS, [lobby_id, user_id, operator_id, bet_amount, win_amount, user_bets, win_pos, color]);
+    const userId = decodeURIComponent(user_id);
+    await write(SQL_INSERT_SETTLEMENTS, [lobby_id, userId, operator_id, bet_amount, win_amount, user_bets, win_pos, color]);
     console.info('Settlement Data Inserted Successfully');
     return;
   } catch (err) {
