e53ac290df640218d0db740ce6a56fdc723e94ed
sushant81074 <sushant94601@gmail.com>
2025-09-18 13:03:12 +0000

    single login done

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,11 @@
 import { Server, Socket } from 'socket.io';
 import { getUserDataFromSource } from './module/players/player-event';
 import { eventRouter } from './router/event-router';
-import { setCache, deleteCache } from './utilities/redis-connection';
+import { setCache, deleteCache, getCache } from './utilities/redis-connection';
+import { createLogger } from './utilities/logger';
 
+const logger = createLogger('Socket');
+  
 export const initSocket = (io: Server): void => {
 
   io.on('connection', async (socket: Socket) => {
@@ -22,6 +25,16 @@ export const initSocket = (io: Server): void => {
       return;
     }
 
+    const isUserExist = await getCache(userData.id);
+    if (isUserExist) {
+        console.log("User already connected from a platform, disconnecting older resource....");
+        const socket = io.sockets.sockets.get(isUserExist);
+        if (socket) {
+            socket.emit('betError', 'User connected from another source');
+            socket.disconnect(true);
+        }
+    }
+
     userData.balance = Number(userData.balance);
     socket.emit('info',
       {
@@ -32,10 +45,13 @@ export const initSocket = (io: Server): void => {
     );
 
     await setCache(`PL:${socket.id}`, JSON.stringify({ ...userData, socketId: socket.id }), 3600);
+    await setCache(userData.id, socket.id);
 
     eventRouter(io, socket);
 
     socket.on('disconnect', async () => {
+      logger.info('Server Disconnected : Deleted the cache of user')
+      await deleteCache(userData.id);
       await deleteCache(`PL:${socket.id}`);
     });
 
